1343d9ca6fb45f96d810d148e4e6544ebd6d2c62
l3acon <hello@kylebaconcreative.com>
2017-07-18 13:49:54 -0700

    Build out styles some more, refactor some html, move some parts around

--- src/js/index.js
+++ src/js/index.js
@@ -1,19 +1,45 @@
 $(document).ready(function() {
-var stickyNavTop = $('.nav').offset().top;
- 
-var stickyNav = function(){
-var scrollTop = $(window).scrollTop();
-      
-if (scrollTop > stickyNavTop) { 
-    $('.nav').addClass('sticky');
-} else {
-    $('.nav').removeClass('sticky'); 
-}
-};
- 
-stickyNav();
- 
-$(window).scroll(function() {
+    var stickyNavTop = $('.nav').offset().top;
+
+    var stickyNav = function(){
+        var scrollTop = $(window).scrollTop();
+
+        if (scrollTop > stickyNavTop) {
+            $('.nav').addClass('sticky');
+        } else {
+            $('.nav').removeClass('sticky');
+        }
+    };
+
     stickyNav();
-});
+
+    $(window).scroll(function() {
+        stickyNav();
+    });
+
+    // Select all links with hashes
+    $('a[href*="#"]')
+    // Remove links that don't actually link to anything
+    .not('[href="#"]')
+    .not('[href="#0"]')
+    .click(function(event) {
+        // On-page links
+        if (
+            location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+            &&
+            location.hostname == this.hostname
+        ) {
+            // Figure out element to scroll to
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            // Does a scroll target exist?
+            if (target.length) {
+                // Only prevent default if animation is actually gonna happen
+                event.preventDefault();
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 500);
+            }
+        }
+    });
 });
